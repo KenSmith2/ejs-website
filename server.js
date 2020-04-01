c7246ab3eeef6dda884b4223c904dc5fb7850f5c
Ken Smith <ken2@onkea.com>
2020-04-01 06:38:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ const app = express();
 app.listen(3000, function() {
     console.log('Ready on port 3000')
 })
+
+app.get('/', function (request, response) {
+    console.log('request on /');
+    response.send('send response to get request')
+})
